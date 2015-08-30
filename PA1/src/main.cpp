1860857a9fef6a0dd51f5c7fdb387d1548f5a29f
Matthew J. Berger <pacoishere@gmail.com>
2015-08-30 13:46:05 -0700

    Add #define GLM_FORCE_RADIANS for compatibility with latest glm library version.

--- PA1/src/main.cpp
+++ PA1/src/main.cpp
@@ -1,3 +1,4 @@
+#define GLM_FORCE_RADIANS
 #include <GL/glew.h> // glew must be included before the main gl libs
 #include <GL/glut.h> // doing otherwise causes compiler shouting
 #include <iostream>
