85d0e4210ace17156f756693a3df212728b23114
Matthew J. Berger <matthewberger@nevada.unr.edu>
2015-09-05 10:15:13 -0700

    Copy Engine code to PA2 folder.

---
+++ PA2/src/program_states/InitialState.h
@@ -0,0 +1,31 @@
+#include "../core/GlutProgram.h"
+
+class InitialState : public ProgramState
+{
+    public:
+        void Initialize(GlutProgram* program);
+        void Finalize();
+
+        void Pause();
+        void Resume();
+
+        void Mouse(int button, int state, int xPos, int yPos);
+        void Keyboard(unsigned char key, int xPos, int yPos);
+        void Reshape(int newWidth, int newHeight);
+        void Render();
+        void Update();
+
+        static InitialState* GetInstance();
+
+    private:
+        InitialState();
+        ~InitialState();
+
+        static InitialState* instance;
+
+        GlutProgram* mainProgram;
+
+        /**************************/
+        /* Place state items here */
+        /**************************/
+};
